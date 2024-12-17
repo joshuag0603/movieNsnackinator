6740dfef78c182deedf796427a8799a3eb3275b7
Silv3rHRT <somethingcuteandgay@gmail.com>
2024-12-17 16:12:02 -0500

    pulling main

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,20 +1,16 @@
-
-const movieButton = document.getElementById('movieButtonEntry');
 let movieList = JSON.parse(localStorage.getItem('movies')) || [];
-console.log('movies list ', movieList)
+console.log('movies list ', movieList);
+
 let snacksList = JSON.parse(localStorage.getItem('snacks'))|| [];
-console.log('snacks list', snacksList)
+console.log('snacks list', snacksList);
 
-
-// Function to display pikachus in the table
-function display() {
-    const moviesList = document.getElementsByTagName('tbody')[0];
-    console.log(moviesList);
-
-    // Clear the table body only if it is the first display
-    if (moviesList.rows.length === 0) {
-        movieList.forEach((movie, index) => {
-            const row = moviesList.insertRow();
+// Function to display movies in the table
+function displayMovies() {
+    const filmTable = document.getElementById('movie-list').getElementsByTagName('tbody')[0];
+    filmTable.innerHTML = ''; //clear table before displaying updated list
+    
+    movieList.forEach((movie, index) => {
+            const row = filmTable.insertRow();
             const cellTitle = row.insertCell(0);
             const cellAction = row.insertCell(1);
 
@@ -22,54 +18,52 @@ function display() {
             cellAction.innerHTML = `<button onclick="removemovie(${index})">Remove</button>`;
         });
     }
-}
 
 // Function to remove a pikachu from the array
 function removemovie(index) {
-    console.log('index?', index)
-    // console.log('movies', movie)
-    movieList.splice(index, 1); // Remove the pikachu from the array
+    // console.log('removing movie at index', index)
+    movieList.splice(index, 1); // Remove the movie from the array
     localStorage.setItem('movies', JSON.stringify(movieList))
-     refreshLists(); 
-  
+    displayMovies(); //refresh table 
 }
 
-// Function to refresh the pikachu list after removal
-function refreshLists() {  
-location.reload()
+// Function to display pikachus in the table
+function displayPantry() {
+    const cupBoard = document.getElementById('pantry-list').getElementsByTagName('tbody')[0];
+    cupBoard.innerHTML = ''; //clear table before displaying updated list
+
+    snacksList.forEach((snack, index) => {
+        const row = cupBoard.insertRow();
+        const cellTitle = row.insertCell(0);
+        const cellAction = row.insertCell(1);
+
+        cellTitle.textContent = snack;
+        cellAction.innerHTML = `<button onclick="removeSnack(${index})">Remove</button>`;
+    });
 }
 
+// Function to remove a snack
+function removeSnack(index) {
+    // console.log('removing snack at index', index):
+    snacksList.splice(index, 1); // Remove the item from the array
+    localStorage.setItem('snacks', JSON.stringify(snacksList)) //updates snack list
+    displayPantry(); // Refreshs the table
+}
+
+//gather and save inventory
 function gatherInventory () {
-
-        // moviesList.push(moviesLogged);
-        localStorage.setItem('movies', JSON.stringify(movieList));
-        // clearMoviesForm();
+    localStorage.setItem('movies', JSON.stringify(movieList));
+    localStorage.setItem('snacks', JSON.stringify(snacksList));
     
-            console.log('Movies form cleared out');
-
-        // moviesList.push(moviesLogged);
-        localStorage.setItem('snacks', JSON.stringify(snacksList));
-        // clearMoviesForm();
-        display()
+    displayMovies();
+    displayPantry();
 
 };
 
 function init() {
-    // Get stored movies from localStorage
-    const storedMovies = JSON.parse(localStorage.getItem('movies'));
-    // If todos were retrieved from localStorage, update the todos array to it
-    if (storedMovies !== null) {
-        movies = storedMovies;
-      }
-    display()
-
-    // const storedSnacks = JSON.parse(localStorage.getItem('snacks'));
-    // // If todos were retrieved from localStorage, update the todos array to it
-    // if (storedSnacks !== null) {
-    //     snacks = storedSnacks;
-    //   }
-    
-    //   display(snack)
+   
+    displayMovies()
+    displayPantry()
 }
 
 
